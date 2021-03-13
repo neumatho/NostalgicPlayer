03e464f8556918fae10aea8ecf3b88ead4e97b98
Thomas Neumann <thomas@polycode.dk>
2021-03-13 08:08:22 +0100

    Changed the project files, so debug and release builds are stored in its own folder in bin.

--- Source/Clients/CopyAgents.bat
+++ Source/Clients/CopyAgents.bat
@@ -2,32 +2,32 @@ mkdir "%~1Agents"
 
 mkdir "%~1Agents\SampleConverters"
 for /r "%~2Agents\SampleConverters" %%f in (*.pdb) do (
-  (echo "%%f" | find /I "\NostalgicPlayerGuiKit.pdb" 1>NUL) || (copy "%%f" "%~1Agents\SampleConverters")
+  (echo "%%f" | find /I "\NostalgicPlayerGuiKit.pdb" 1>NUL) || (echo "%%f" | find /I /V "\%~3\" 1>NUL) || (copy "%%f" "%~1Agents\SampleConverters")
 )
 for /r "%~2Agents\SampleConverters" %%f in (*.dll) do (
-  (echo "%%f" | find /I "\obj\" 1>NUL) || (echo "%%f" | find /I "\ref\" 1>NUL) || (echo "%%f" | find /I "\NostalgicPlayerGuiKit.dll" 1>NUL) || (copy "%%f" "%~1Agents\SampleConverters")
+  (echo "%%f" | find /I "\obj\" 1>NUL) || (echo "%%f" | find /I "\ref\" 1>NUL) || (echo "%%f" | find /I /V "\%~3\" 1>NUL) || (echo "%%f" | find /I "\NostalgicPlayerGuiKit.dll" 1>NUL) || (copy "%%f" "%~1Agents\SampleConverters")
 )
 
 mkdir "%~1Agents\Output"
 for /r "%~2Agents\Output" %%f in (*.pdb) do (
-  (echo "%%f" | find /I "\NostalgicPlayerGuiKit.pdb" 1>NUL) || (copy "%%f" "%~1Agents\Output")
+  (echo "%%f" | find /I "\NostalgicPlayerGuiKit.pdb" 1>NUL) || (echo "%%f" | find /I /V "\%~3\" 1>NUL) || (copy "%%f" "%~1Agents\Output")
 )
 for /r "%~2Agents\Output" %%f in (*.dll) do (
-  (echo "%%f" | find /I "\obj\" 1>NUL) || (echo "%%f" | find /I "\ref\" 1>NUL) || (echo "%%f" | find /I "\NostalgicPlayerGuiKit.dll" 1>NUL) || (copy "%%f" "%~1Agents\Output")
+  (echo "%%f" | find /I "\obj\" 1>NUL) || (echo "%%f" | find /I "\ref\" 1>NUL) || (echo "%%f" | find /I /V "\%~3\" 1>NUL) || (echo "%%f" | find /I "\NostalgicPlayerGuiKit.dll" 1>NUL) || (copy "%%f" "%~1Agents\Output")
 )
 
 mkdir "%~1Agents\Players"
 for /r "%~2Agents\Players" %%f in (*.pdb) do (
-  (echo "%%f" | find /I "\NostalgicPlayerGuiKit.pdb" 1>NUL) || (copy "%%f" "%~1Agents\Players")
+  (echo "%%f" | find /I "\NostalgicPlayerGuiKit.pdb" 1>NUL) || (echo "%%f" | find /I /V "\%~3\" 1>NUL) || (copy "%%f" "%~1Agents\Players")
 )
 for /r "%~2Agents\Players" %%f in (*.dll) do (
-  (echo "%%f" | find /I "\obj\" 1>NUL) || (echo "%%f" | find /I "\ref\" 1>NUL) || (echo "%%f" | find /I "\NostalgicPlayerGuiKit.dll" 1>NUL) || (copy "%%f" "%~1Agents\Players")
+  (echo "%%f" | find /I "\obj\" 1>NUL) || (echo "%%f" | find /I "\ref\" 1>NUL) || (echo "%%f" | find /I /V "\%~3\" 1>NUL) || (echo "%%f" | find /I "\NostalgicPlayerGuiKit.dll" 1>NUL) || (copy "%%f" "%~1Agents\Players")
 )
 
 mkdir "%~1Agents\Visuals"
 for /r "%~2Agents\Visuals" %%f in (*.pdb) do (
-  (echo "%%f" | find /I "\NostalgicPlayerGuiKit.pdb" 1>NUL) || (copy "%%f" "%~1Agents\Visuals")
+  (echo "%%f" | find /I "\NostalgicPlayerGuiKit.pdb" 1>NUL) || (echo "%%f" | find /I /V "\%~3\" 1>NUL) || (copy "%%f" "%~1Agents\Visuals")
 )
 for /r "%~2Agents\Visuals" %%f in (*.dll) do (
-  (echo "%%f" | find /I "\obj\" 1>NUL) || (echo "%%f" | find /I "\ref\" 1>NUL) || (echo "%%f" | find /I "\NostalgicPlayerGuiKit.dll" 1>NUL) || (copy "%%f" "%~1Agents\Visuals")
+  (echo "%%f" | find /I "\obj\" 1>NUL) || (echo "%%f" | find /I "\ref\" 1>NUL) || (echo "%%f" | find /I /V "\%~3\" 1>NUL) || (echo "%%f" | find /I "\NostalgicPlayerGuiKit.dll" 1>NUL) || (copy "%%f" "%~1Agents\Visuals")
 )
