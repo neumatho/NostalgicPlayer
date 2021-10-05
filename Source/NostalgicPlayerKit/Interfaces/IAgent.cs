d188f60593350eb633baa828eaa7b3ae0f9e2994
Thomas Neumann <thomas@polycode.dk>
2021-10-05 15:15:59 +0200

    Prepared for release.

--- Source/NostalgicPlayerKit/Interfaces/IAgent.cs
+++ Source/NostalgicPlayerKit/Interfaces/IAgent.cs
@@ -19,7 +19,7 @@ namespace Polycode.NostalgicPlayer.Kit.Interfaces
 		/// <summary>
 		/// 1.3.0
 		/// </summary>
-		public const int NostalgicPlayer_Current_Version = (1 << 16) + (3 << 8) + 0;
+		public const int NostalgicPlayer_Current_Version = (1 << 16) + (4 << 8) + 0;
 
 		/// <summary>
 		/// Return the NostalgicPlayer_Current_Version constant defined above
