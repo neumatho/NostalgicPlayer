8cfe0b968c5702e497237d1317de73ad945fa3ab
Thomas Neumann <thomas@polycode.dk>
2024-12-01 06:13:35 +0100

    Updated Ancient test cases.

--- Source/Ports/Tests/Ancient.Test/Test.cs
+++ Source/Ports/Tests/Ancient.Test/Test.cs
@@ -3,13 +3,7 @@
 /* license of NostalgicPlayer is keep. See the LICENSE file for more          */
 /* information.                                                               */
 /******************************************************************************/
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Reflection;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Polycode.NostalgicPlayer.Ports.Ancient;
-using Polycode.NostalgicPlayer.Ports.Ancient.Exceptions;
 
 namespace Polycode.NostalgicPlayer.Ports.Tests.Ancient.Test
 {
@@ -17,8 +11,19 @@ namespace Polycode.NostalgicPlayer.Ports.Tests.Ancient.Test
 	/// 
 	/// </summary>
 	[TestClass]
-	public class Test
+	public class Test : TestBase
 	{
+		/********************************************************************/
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/********************************************************************/
+		public Test() : base("Test_Files")
+		{
+		}
+
+
+
 		/********************************************************************/
 		/// <summary>
 		/// 
@@ -136,112 +141,5 @@ namespace Polycode.NostalgicPlayer.Ports.Tests.Ancient.Test
 			VerifyFile("test_C1_smpl.xpkf", "test_C1.raw");
 			VerifyFile("test_C1_sqsh.xpkf", "test_C1.raw");
 		}
-
-		#region Private methods
-		/********************************************************************/
-		/// <summary>
-		/// 
-		/// </summary>
-		/********************************************************************/
-		private byte[] ReadFile(string fileName)
-		{
-			string location = Path.GetDirectoryName(Assembly.GetCallingAssembly().Location);
-
-			using (FileStream fs = File.OpenRead(Path.Combine(location, "..\\..\\..\\Test_Files", fileName)))
-			{
-				byte[] buffer = new byte[fs.Length];
-				fs.ReadExactly(buffer);
-
-				return buffer;
-			}
-		}
-
-
-
-		/********************************************************************/
-		/// <summary>
-		/// 
-		/// </summary>
-		/********************************************************************/
-		private void VerifyFile(string packedFile, byte[] verify, bool ignoreExpansion = false)
-		{
-			using (MemoryStream packed = new MemoryStream(ReadFile(packedFile)))
-			{
-				Decompressor decompressor;
-
-				try
-				{
-					decompressor = new Decompressor(packed);
-				}
-				catch (InvalidFormatException)
-				{
-					Assert.Fail($"Unknown or invalid compression format in file {packedFile}");
-					return;
-				}
-				catch (VerificationException)
-				{
-					Assert.Fail($"Verify (packed) failed for {packedFile}");
-					return;
-				}
-
-				List<byte[]> raw = new List<byte[]>();
-
-				try
-				{
-					raw.AddRange(decompressor.Decompress());
-				}
-				catch (DecompressionException)
-				{
-					Assert.Fail($"Decompression failed for {packedFile}");
-					return;
-				}
-				catch (VerificationException)
-				{
-					Assert.Fail($"Verify (raw) failed for {packedFile}");
-					return;
-				}
-
-				int actualSize = raw.Sum(x => x.Length);
-				if (((verify.Length != actualSize) && !ignoreExpansion) || ((actualSize < verify.Length) && ignoreExpansion))
-				{
-					Assert.Fail($"Verify failed for {packedFile} - size differs from original");
-					return;
-				}
-
-				int offset = 0;
-
-				byte[] rawToCheck = null;
-				int rawIndex = 0;
-
-				for (int i = 0, j = 0; i < verify.Length; i++, j++)
-				{
-					if ((rawToCheck == null) || (j == rawToCheck.Length))
-					{
-						rawToCheck = raw[rawIndex++];
-						j = 0;
-					}
-
-					if (rawToCheck[j] != verify[i + offset])
-					{
-						Assert.Fail($"Verify failed for {packedFile} - contents differ @ {i} from original");
-						return;
-					}
-				}
-			}
-		}
-
-
-
-		/********************************************************************/
-		/// <summary>
-		/// 
-		/// </summary>
-		/********************************************************************/
-		private void VerifyFile(string packedFile, string rawFile, bool ignoreExpansion = false)
-		{
-			byte[] verify = ReadFile(rawFile);
-			VerifyFile(packedFile, verify, ignoreExpansion);
-		}
-		#endregion
 	}
 }
