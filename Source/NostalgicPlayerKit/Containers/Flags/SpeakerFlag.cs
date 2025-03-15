2e60e0b94edce6e207093150aaec5c75aad9bbae
Thomas Neumann <thomas@polycode.dk>
2025-03-15 14:34:54 +0100

    Added missing comments.

--- Source/NostalgicPlayerKit/Containers/Flags/SpeakerFlag.cs
+++ Source/NostalgicPlayerKit/Containers/Flags/SpeakerFlag.cs
@@ -13,23 +13,41 @@ namespace Polycode.NostalgicPlayer.Kit.Containers.Flags
 	[Flags]
 	public enum SpeakerFlag
 	{
+		/// <summary></summary>
 		FrontLeft = 0x00001,
+		/// <summary></summary>
 		FrontRight = 0x00002,
+		/// <summary></summary>
 		FrontCenter = 0x00004,
+		/// <summary></summary>
 		LowFrequency = 0x00008,
+		/// <summary></summary>
 		BackLeft = 0x00010,
+		/// <summary></summary>
 		BackRight = 0x00020,
+		/// <summary></summary>
 		FrontLeftOfCenter = 0x00040,
+		/// <summary></summary>
 		FrontRightOfCenter = 0x00080,
+		/// <summary></summary>
 		BackCenter = 0x00100,
+		/// <summary></summary>
 		SideLeft = 0x00200,
+		/// <summary></summary>
 		SideRight = 0x00400,
+		/// <summary></summary>
 		TopCenter = 0x00800,
+		/// <summary></summary>
 		TopFrontLeft = 0x01000,
+		/// <summary></summary>
 		TopFrontCenter = 0x02000,
+		/// <summary></summary>
 		TopFrontRight = 0x04000,
+		/// <summary></summary>
 		TopBackLeft = 0x08000,
+		/// <summary></summary>
 		TopBackCenter = 0x10000,
+		/// <summary></summary>
 		TopBackRight = 0x20000
 	}
 }
