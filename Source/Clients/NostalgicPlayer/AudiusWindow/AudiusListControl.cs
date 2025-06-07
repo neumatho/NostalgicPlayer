b87114a4beb84d5327b0b73db4b972a08fb5c524
Thomas Neumann <thomas@polycode.dk>
2025-06-07 11:05:13 +0200

    Created the Audius list item control, which can show a list of items containing different information about the item.

---
+++ Source/Clients/NostalgicPlayer/AudiusWindow/AudiusListControl.cs
@@ -0,0 +1,135 @@
+﻿/******************************************************************************/
+/* This source, or parts thereof, may be used in any software as long the     */
+/* license of NostalgicPlayer is keep. See the LICENSE file for more          */
+/* information.                                                               */
+/******************************************************************************/
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Polycode.NostalgicPlayer.Client.GuiPlayer.AudiusWindow
+{
+	/// <summary>
+	/// Shows a list of Audius items, such as tracks, playlists, etc.
+	/// </summary>
+	public partial class AudiusListControl : UserControl
+	{
+		/********************************************************************/
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/********************************************************************/
+		public AudiusListControl()
+		{
+			InitializeComponent();
+		}
+
+		#region Public methods
+		/********************************************************************/
+		/// <summary>
+		/// Initialize the list with new items
+		/// </summary>
+		/********************************************************************/
+		public void SetItems(IEnumerable<AudiusListItem> items)
+		{
+			// First remove and dispose all existing items
+			foreach (Control ctrl in flowLayoutPanel.Controls)
+				ctrl.Dispose();
+
+			flowLayoutPanel.Controls.Clear();
+
+			// Then add the new items
+			foreach (AudiusListItem item in items)
+			{
+				AudiusListItemControl ctrl = new AudiusListItemControl(item);
+				ctrl.Width = flowLayoutPanel.ClientSize.Width - ctrl.Margin.Horizontal;
+
+				flowLayoutPanel.Controls.Add(ctrl);
+			}
+		}
+		#endregion
+
+		#region Event handlers
+		/********************************************************************/
+		/// <summary>
+		/// Is called when the flow layout resizes
+		/// </summary>
+		/********************************************************************/
+		private void FlowLayout_Resize(object sender, EventArgs e)
+		{
+			foreach (Control ctrl in flowLayoutPanel.Controls)
+				ctrl.Width = flowLayoutPanel.ClientSize.Width - ctrl.Margin.Horizontal;
+		}
+
+
+
+		/********************************************************************/
+		/// <summary>
+		/// Is called when the flow layout scrolls
+		/// </summary>
+		/********************************************************************/
+		private void FlowLayout_Scroll(object sender, ScrollEventArgs e)
+		{
+			UpdateVisibleItems();
+		}
+		#endregion
+
+		#region Private methods
+		/********************************************************************/
+		/// <summary>
+		/// Update visible items if needed
+		/// </summary>
+		/********************************************************************/
+		private void UpdateVisibleItems()
+		{
+			int i;
+
+			int startYOfVisibleArea = Math.Abs(flowLayoutPanel.AutoScrollPosition.Y);
+			int endYOfVisibleArea = startYOfVisibleArea + flowLayoutPanel.Height;
+
+			int itemYPosition = 0;
+
+			// First skip all items that are not visible (has been scrolled up)
+			for (i = 0; i < flowLayoutPanel.Controls.Count; i++)
+			{
+				AudiusListItemControl item = (AudiusListItemControl)flowLayoutPanel.Controls[i];
+				int itemHeight = item.Height + item.Margin.Vertical;
+
+				// Stop loop if item is visible
+				if (IsItemVisible(startYOfVisibleArea, endYOfVisibleArea, itemYPosition, itemYPosition + itemHeight))
+					break;
+
+				itemYPosition += itemHeight;
+			}
+
+			// Update all visible items
+			for (; i < flowLayoutPanel.Controls.Count; i++)
+			{
+				AudiusListItemControl item = (AudiusListItemControl)flowLayoutPanel.Controls[i];
+				int itemHeight = item.Height + item.Margin.Vertical;
+
+				// If the item is not visible, then stop the loop
+				if (!IsItemVisible(startYOfVisibleArea, endYOfVisibleArea, itemYPosition, itemYPosition + itemHeight))
+					break;
+
+				item.RefreshItem();
+
+				itemYPosition += itemHeight;
+			}
+		}
+
+
+
+		/********************************************************************/
+		/// <summary>
+		/// Will find out if an item is visible in the current view
+		/// </summary>
+		/********************************************************************/
+		private bool IsItemVisible(int visibleStart, int visibleEnd, int itemStart, int itemEnd)
+		{
+			return (itemStart < visibleEnd) && (itemEnd > visibleStart);
+		}
+		#endregion
+	}
+}
---
+++ Source/Clients/NostalgicPlayer/AudiusWindow/AudiusListItem.cs
@@ -0,0 +1,115 @@
+﻿/******************************************************************************/
+/* This source, or parts thereof, may be used in any software as long the     */
+/* license of NostalgicPlayer is keep. See the LICENSE file for more          */
+/* information.                                                               */
+/******************************************************************************/
+using System;
+
+namespace Polycode.NostalgicPlayer.Client.GuiPlayer.AudiusWindow
+{
+	/// <summary>
+	/// This class is used for each item in an Audius list
+	/// </summary>
+	public class AudiusListItem
+	{
+		/********************************************************************/
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/********************************************************************/
+		public AudiusListItem(int position, string title, string artist, TimeSpan duration, int reposts, int favorites, int plays)
+		{
+			Position = position;
+			Title = title;
+			Artist = artist;
+			Duration = duration;
+			Reposts = reposts;
+			Favorites = favorites;
+			Plays = plays;
+		}
+
+
+
+		/********************************************************************/
+		/// <summary>
+		/// Return the position of the item
+		/// </summary>
+		/********************************************************************/
+		public int Position
+		{
+			get;
+		}
+
+
+
+		/********************************************************************/
+		/// <summary>
+		/// Returns the title of the item
+		/// </summary>
+		/********************************************************************/
+		public string Title
+		{
+			get;
+		}
+
+
+
+		/********************************************************************/
+		/// <summary>
+		/// Returns the artist of the item
+		/// </summary>
+		/********************************************************************/
+		public string Artist
+		{
+			get;
+		}
+
+
+
+		/********************************************************************/
+		/// <summary>
+		/// Returns the total duration of the item
+		/// </summary>
+		/********************************************************************/
+		public TimeSpan Duration
+		{
+			get;
+		}
+
+
+
+		/********************************************************************/
+		/// <summary>
+		/// Returns the number of reposts for the item
+		/// </summary>
+		/********************************************************************/
+		public int Reposts
+		{
+			get;
+		}
+
+
+
+		/********************************************************************/
+		/// <summary>
+		/// Returns the number of favorites for the item
+		/// </summary>
+		/********************************************************************/
+		public int Favorites
+		{
+			get;
+		}
+
+
+
+		/********************************************************************/
+		/// <summary>
+		/// Returns the number of plays for the item
+		/// </summary>
+		/********************************************************************/
+		public int Plays
+		{
+			get;
+		}
+	}
+}
