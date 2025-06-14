0c92ee265aefbb9e94a77f78b69745edc8a92e5c
Thomas Neumann <thomas@polycode.dk>
2025-06-14 14:38:05 +0200

    Now cover picture and other track information is shown in Module Information window when playing an Audius track.

--- Source/NostalgicPlayerLibrary/Containers/ModuleInfoStatic.cs
+++ Source/NostalgicPlayerLibrary/Containers/ModuleInfoStatic.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using Polycode.NostalgicPlayer.Kit.Containers;
 using Polycode.NostalgicPlayer.Kit.Containers.Flags;
 using Polycode.NostalgicPlayer.Kit.Interfaces;
+using Polycode.NostalgicPlayer.PlayerLibrary.Interfaces;
 using Polycode.NostalgicPlayer.PlayerLibrary.Loaders;
 
 namespace Polycode.NostalgicPlayer.PlayerLibrary.Containers
@@ -129,6 +130,22 @@ namespace Polycode.NostalgicPlayer.PlayerLibrary.Containers
 		/********************************************************************/
 		internal ModuleInfoStatic(StreamLoader loader, IStreamerAgent streamerAgent) : this(loader, (IModuleInformation)streamerAgent)
 		{
+			if (loader is IStreamMetadata streamMetadata)
+			{
+				// If the loader supports stream metadata, then we can get some extra information
+				ModuleName = streamMetadata.Title;
+				Author = streamMetadata.Author;
+				Comment = streamMetadata.Comment;
+				Pictures = streamMetadata.Pictures;
+			}
+			else
+			{
+				ModuleName = string.Empty;
+				Author = string.Empty;
+				Comment = null;
+				Pictures = null;
+			}
+
 			Channels = streamerAgent.ChannelCount;
 			VirtualChannels = streamerAgent.ChannelCount;
 			MaxSongNumber = 1;
---
+++ Source/NostalgicPlayerLibrary/Interfaces/IStreamMetadata.cs
@@ -0,0 +1,41 @@
+﻿/******************************************************************************/
+/* This source, or parts thereof, may be used in any software as long the     */
+/* license of NostalgicPlayer is keep. See the LICENSE file for more          */
+/* information.                                                               */
+/******************************************************************************/
+using System;
+using Polycode.NostalgicPlayer.Kit.Containers;
+
+namespace Polycode.NostalgicPlayer.PlayerLibrary.Interfaces
+{
+	/// <summary>
+	/// Loaders which implement this interface can provide metadata about the stream
+	/// </summary>
+	public interface IStreamMetadata
+	{
+		/// <summary>
+		/// Return the title of the song
+		/// </summary>
+		string Title { get; }
+
+		/// <summary>
+		/// Return the name of the author
+		/// </summary>
+		string Author { get; }
+
+		/// <summary>
+		/// Return some comment about the song
+		/// </summary>
+		string[] Comment { get; }
+
+		/// <summary>
+		/// Holds the duration of the module this item has
+		/// </summary>
+		public TimeSpan Duration { get; }
+
+		/// <summary>
+		/// Return all pictures available
+		/// </summary>
+		PictureInfo[] Pictures { get; }
+	}
+}
