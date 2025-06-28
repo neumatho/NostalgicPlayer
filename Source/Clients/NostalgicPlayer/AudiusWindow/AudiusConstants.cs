8db80bfc52fcae7dfa766dde65582ed51ef9b0b5
Thomas Neumann <thomas@polycode.dk>
2025-06-28 09:55:10 +0200

    Fixed retrieving of long playlists.

---
+++ Source/Clients/NostalgicPlayer/AudiusWindow/AudiusConstants.cs
@@ -0,0 +1,19 @@
+﻿/******************************************************************************/
+/* This source, or parts thereof, may be used in any software as long the     */
+/* license of NostalgicPlayer is keep. See the LICENSE file for more          */
+/* information.                                                               */
+/******************************************************************************/
+namespace Polycode.NostalgicPlayer.Client.GuiPlayer.AudiusWindow
+{
+	/// <summary>
+	/// Different constants used by Audius
+	/// </summary>
+	public static class AudiusConstants
+	{
+		/// <summary></summary>
+		public const int MaxSearchResults = 100;
+
+		/// <summary></summary>
+		public const int MaxTracksPerPlaylist = 150;
+	}
+}
