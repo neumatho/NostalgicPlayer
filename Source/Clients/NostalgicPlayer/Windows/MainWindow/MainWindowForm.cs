98fb72f95e9884e67e1a5c808f7f6ec230fbc209
Thomas Neumann <thomas@polycode.dk>
2025-08-24 08:10:26 +0200

    Created own Form control which renders itself. Updated FavoriteSongSystem window to use this new form.

--- Source/Clients/NostalgicPlayer/Windows/MainWindow/MainWindowForm.cs
+++ Source/Clients/NostalgicPlayer/Windows/MainWindow/MainWindowForm.cs
@@ -1416,7 +1416,7 @@ namespace Polycode.NostalgicPlayer.Client.GuiPlayer.Windows.MainWindow
 					aboutWindow.Close();
 
 				// Minimize all windows
-				foreach (WindowFormBase window in GetAllOpenedWindows())
+				foreach (IWindowForm window in GetAllOpenedWindows())
 				{
 					window.UpdateWindowSettings();
 					window.WindowState = FormWindowState.Minimized;
@@ -1425,7 +1425,7 @@ namespace Polycode.NostalgicPlayer.Client.GuiPlayer.Windows.MainWindow
 			else if (WindowState == FormWindowState.Normal)
 			{
 				// Open all windows
-				foreach (WindowFormBase window in GetAllOpenedWindows())
+				foreach (IWindowForm window in GetAllOpenedWindows())
 					window.WindowState = FormWindowState.Normal;
 			}
 		}
@@ -3634,7 +3634,7 @@ namespace Polycode.NostalgicPlayer.Client.GuiPlayer.Windows.MainWindow
 		/// Get all opened windows
 		/// </summary>
 		/********************************************************************/
-		private IEnumerable<WindowFormBase> GetAllOpenedWindows()
+		private IEnumerable<IWindowForm> GetAllOpenedWindows()
 		{
 			if (IsHelpWindowOpen())
 				yield return helpWindow;
---
+++ Source/NostalgicPlayer.Controls/Native/TRACKMOUSEEVENT.cs
@@ -0,0 +1,24 @@
+﻿/******************************************************************************/
+/* This source, or parts thereof, may be used in any software as long the     */
+/* license of NostalgicPlayer is keep. See the LICENSE file for more          */
+/* information.                                                               */
+/******************************************************************************/
+using System;
+using System.Runtime.InteropServices;
+
+namespace Polycode.NostalgicPlayer.Controls.Native
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	[StructLayout(LayoutKind.Sequential)]
+	// ReSharper disable InconsistentNaming
+	internal struct TRACKMOUSEEVENT
+	{
+		public uint cbSize;
+		public uint dwFlags;
+		public IntPtr hwndTrack;
+		public uint dwHoverTime;
+	}
+	// ReSharper restore InconsistentNaming
+}
