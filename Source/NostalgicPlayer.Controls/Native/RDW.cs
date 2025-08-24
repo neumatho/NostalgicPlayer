16573ffa687a08950dd3e84c67a75098630ef32f
Thomas Neumann <thomas@polycode.dk>
2025-08-24 08:10:26 +0200

    Created own Form control which renders itself. Updated FavoriteSongSystem window to use this new form.

---
+++ Source/NostalgicPlayer.Controls/Native/RDW.cs
@@ -0,0 +1,22 @@
+﻿/******************************************************************************/
+/* This source, or parts thereof, may be used in any software as long the     */
+/* license of NostalgicPlayer is keep. See the LICENSE file for more          */
+/* information.                                                               */
+/******************************************************************************/
+using System;
+
+namespace Polycode.NostalgicPlayer.Controls.Native
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	// ReSharper disable InconsistentNaming
+	[Flags]
+	internal enum RDW : uint
+	{
+		INVALIDATE = 0x0001,
+		NOCHILDREN = 0x0040,
+		FRAME = 0x0400,
+	}
+	// ReSharper restore InconsistentNaming
+}
