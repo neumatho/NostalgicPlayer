48942616e16a5f9c14ae3b2d71e2e506dbbe65f4
Thomas Neumann <thomas@polycode.dk>
2025-09-13 14:06:30 +0200

    Added theme support.

---
+++ Source/NostalgicPlayer.Controls/Theme/Interfaces/IThemeManager.cs
@@ -0,0 +1,30 @@
+﻿/******************************************************************************/
+/* This source, or parts thereof, may be used in any software as long the     */
+/* license of NostalgicPlayer is keep. See the LICENSE file for more          */
+/* information.                                                               */
+/******************************************************************************/
+using Polycode.NostalgicPlayer.Controls.Containers.Events;
+
+namespace Polycode.NostalgicPlayer.Controls.Theme.Interfaces
+{
+	/// <summary>
+	/// Theme manager interface
+	/// </summary>
+	public interface IThemeManager
+	{
+		/// <summary>
+		/// Is called when the theme changes
+		/// </summary>
+		event ThemeChangedEventHandler ThemeChanged;
+
+		/// <summary>
+		/// Return the current theme
+		/// </summary>
+		public ITheme CurrentTheme { get; }
+
+		/// <summary>
+		/// Switch to the new theme given
+		/// </summary>
+		public void SwitchTheme(ITheme newTheme);
+	}
+}
