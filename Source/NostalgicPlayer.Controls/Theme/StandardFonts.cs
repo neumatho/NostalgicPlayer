49349d5b4055624ab84b729e5891386ae1661145
Thomas Neumann <thomas@polycode.dk>
2025-09-13 14:06:30 +0200

    Added theme support.

---
+++ Source/NostalgicPlayer.Controls/Theme/StandardFonts.cs
@@ -0,0 +1,76 @@
+﻿/******************************************************************************/
+/* This source, or parts thereof, may be used in any software as long the     */
+/* license of NostalgicPlayer is keep. See the LICENSE file for more          */
+/* information.                                                               */
+/******************************************************************************/
+using System;
+using System.Drawing;
+using Polycode.NostalgicPlayer.Controls.Theme.Interfaces;
+
+namespace Polycode.NostalgicPlayer.Controls.Theme
+{
+	/// <summary>
+	/// Returns a collection of standard fonts
+	/// </summary>
+	internal class StandardFonts : IFonts, IDisposable
+	{
+		private const float DefaultFontSize = 8.0f;
+
+		private Font regularFont;
+		private Font monospaceFont;
+
+		/********************************************************************/
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/********************************************************************/
+		public StandardFonts()
+		{
+			regularFont = new Font("Microsoft Sans", DefaultFontSize, FontStyle.Regular, GraphicsUnit.Point);
+			monospaceFont = new Font("Lucida Console", DefaultFontSize, FontStyle.Regular, GraphicsUnit.Point);
+		}
+
+
+
+		/********************************************************************/
+		/// <summary>
+		/// Cleanup
+		/// </summary>
+		/********************************************************************/
+		public void Dispose()
+		{
+			regularFont.Dispose();
+			monospaceFont.Dispose();
+
+			regularFont = null;
+			monospaceFont = null;
+		}
+
+
+
+		/********************************************************************/
+		/// <summary>
+		/// 
+		/// </summary>
+		/********************************************************************/
+		public Font FormTitleFont => regularFont;
+
+
+
+		/********************************************************************/
+		/// <summary>
+		/// 
+		/// </summary>
+		/********************************************************************/
+		public Font RegularFont => regularFont;
+
+
+
+		/********************************************************************/
+		/// <summary>
+		/// 
+		/// </summary>
+		/********************************************************************/
+		public Font MonospaceFont => monospaceFont;
+	}
+}
---
+++ Source/NostalgicPlayer.Controls/Theme/ThemeManagerRegister.cs
@@ -0,0 +1,27 @@
+﻿/******************************************************************************/
+/* This source, or parts thereof, may be used in any software as long the     */
+/* license of NostalgicPlayer is keep. See the LICENSE file for more          */
+/* information.                                                               */
+/******************************************************************************/
+using Microsoft.Extensions.DependencyInjection;
+using Polycode.NostalgicPlayer.Controls.Theme.Interfaces;
+
+namespace Polycode.NostalgicPlayer.Controls.Theme
+{
+	/// <summary>
+	/// Register class
+	/// </summary>
+	public static class ThemeManagerRegister
+	{
+		/********************************************************************/
+		/// <summary>
+		/// Make sure all needed classes are registered in the dependency
+		/// injection collection
+		/// </summary>
+		/********************************************************************/
+		public static void RegisterThemeManager(this IServiceCollection serviceCollection)
+		{
+			serviceCollection.AddSingleton<IThemeManager, ThemeManager>();
+		}
+	}
+}
