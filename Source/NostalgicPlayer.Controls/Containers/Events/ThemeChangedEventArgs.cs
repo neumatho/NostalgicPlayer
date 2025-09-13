f02bd248a02e823f19bd0a501eb36f179f10f84e
Thomas Neumann <thomas@polycode.dk>
2025-09-13 14:06:30 +0200

    Added theme support.

---
+++ Source/NostalgicPlayer.Controls/Containers/Events/ThemeChangedEventArgs.cs
@@ -0,0 +1,41 @@
+﻿/******************************************************************************/
+/* This source, or parts thereof, may be used in any software as long the     */
+/* license of NostalgicPlayer is keep. See the LICENSE file for more          */
+/* information.                                                               */
+/******************************************************************************/
+using System;
+using Polycode.NostalgicPlayer.Controls.Theme.Interfaces;
+
+namespace Polycode.NostalgicPlayer.Controls.Containers.Events
+{
+	/// <summary></summary>
+	public delegate void ThemeChangedEventHandler(object sender, ThemeChangedEventArgs e);
+
+	/// <summary>
+	/// Event class holding needed information when sending a theme changed event
+	/// </summary>
+	public class ThemeChangedEventArgs : EventArgs
+	{
+		/********************************************************************/
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/********************************************************************/
+		internal ThemeChangedEventArgs(ITheme newTheme)
+		{
+			NewTheme = newTheme;
+		}
+
+
+
+		/********************************************************************/
+		/// <summary>
+		/// Holding the new theme
+		/// </summary>
+		/********************************************************************/
+		public ITheme NewTheme
+		{
+			get;
+		}
+	}
+}
---
+++ Source/NostalgicPlayer.Controls/Theme/ThemeManager.cs
@@ -0,0 +1,83 @@
+﻿/******************************************************************************/
+/* This source, or parts thereof, may be used in any software as long the     */
+/* license of NostalgicPlayer is keep. See the LICENSE file for more          */
+/* information.                                                               */
+/******************************************************************************/
+using System;
+using Polycode.NostalgicPlayer.Controls.Containers.Events;
+using Polycode.NostalgicPlayer.Controls.Theme.Interfaces;
+using Polycode.NostalgicPlayer.Controls.Theme.Office2010Blue;
+
+namespace Polycode.NostalgicPlayer.Controls.Theme
+{
+	/// <summary>
+	/// This class controls and manages all the themes and make
+	/// sure that forms and controls are updated when the theme
+	/// changes
+	/// </summary>
+	internal class ThemeManager : IThemeManager, IDisposable
+	{
+		private ITheme theme;
+
+		/********************************************************************/
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/********************************************************************/
+		public ThemeManager()
+		{
+			theme = new Office2010BlueTheme();
+		}
+
+
+
+		/********************************************************************/
+		/// <summary>
+		/// Cleanup
+		/// </summary>
+		/********************************************************************/
+		public void Dispose()
+		{
+			if (theme is IDisposable disposable)
+				disposable.Dispose();
+
+			theme = null;
+		}
+
+
+
+		/********************************************************************/
+		/// <summary>
+		/// Is called when the theme changes
+		/// </summary>
+		/********************************************************************/
+		public event ThemeChangedEventHandler ThemeChanged;
+
+
+
+		/********************************************************************/
+		/// <summary>
+		/// Return the current theme
+		/// </summary>
+		/********************************************************************/
+		public ITheme CurrentTheme => theme;
+
+
+
+		/********************************************************************/
+		/// <summary>
+		/// Switch to the new theme given
+		/// </summary>
+		/********************************************************************/
+		public void SwitchTheme(ITheme newTheme)
+		{
+			if (theme is IDisposable disposable)
+				disposable.Dispose();
+
+			theme = newTheme;
+
+			if (ThemeChanged != null)
+				ThemeChanged(this, new ThemeChangedEventArgs(theme));
+		}
+	}
+}
