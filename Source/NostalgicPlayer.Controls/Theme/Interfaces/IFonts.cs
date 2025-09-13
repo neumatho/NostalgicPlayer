e67e0141c8486f1f50a7a914970b5927f06107df
Thomas Neumann <thomas@polycode.dk>
2025-09-13 14:06:30 +0200

    Added theme support.

---
+++ Source/NostalgicPlayer.Controls/Theme/Interfaces/IFonts.cs
@@ -0,0 +1,24 @@
+﻿/******************************************************************************/
+/* This source, or parts thereof, may be used in any software as long the     */
+/* license of NostalgicPlayer is keep. See the LICENSE file for more          */
+/* information.                                                               */
+/******************************************************************************/
+using System.Drawing;
+
+namespace Polycode.NostalgicPlayer.Controls.Theme.Interfaces
+{
+	/// <summary>
+	/// Holds all the fonts
+	/// </summary>
+	public interface IFonts
+	{
+		/// <summary></summary>
+		Font FormTitleFont { get; }
+
+		/// <summary></summary>
+		Font RegularFont { get; }
+
+		/// <summary></summary>
+		Font MonospaceFont { get; }
+	}
+}
