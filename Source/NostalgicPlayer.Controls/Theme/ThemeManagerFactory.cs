a066f7d0883f4b75db956f20ce83a2e3ffce64a4
Thomas Neumann <thomas@polycode.dk>
2025-09-25 15:46:33 +0200

    Removed dependency injection and created a factory class instead.

--- Source/NostalgicPlayer.Controls/Theme/ThemeManagerFactory.cs
+++ Source/NostalgicPlayer.Controls/Theme/ThemeManagerFactory.cs
@@ -3,25 +3,29 @@
 /* license of NostalgicPlayer is keep. See the LICENSE file for more          */
 /* information.                                                               */
 /******************************************************************************/
-using Microsoft.Extensions.DependencyInjection;
 using Polycode.NostalgicPlayer.Controls.Theme.Interfaces;
 
 namespace Polycode.NostalgicPlayer.Controls.Theme
 {
 	/// <summary>
-	/// Register class
+	/// Factory class for theme manager
 	/// </summary>
-	public static class ThemeManagerRegister
+	public static class ThemeManagerFactory
 	{
+		private static IThemeManager themeManager;
+
 		/********************************************************************/
 		/// <summary>
-		/// Make sure all needed classes are registered in the dependency
-		/// injection collection
+		/// Create a new instance of the theme manager if not created already
+		/// and return the instance
 		/// </summary>
 		/********************************************************************/
-		public static void RegisterThemeManager(this IServiceCollection serviceCollection)
+		public static IThemeManager GetThemeManager()
 		{
-			serviceCollection.AddSingleton<IThemeManager, ThemeManager>();
+			if (themeManager == null)
+				themeManager = new ThemeManager();
+
+			return themeManager;
 		}
 	}
 }
