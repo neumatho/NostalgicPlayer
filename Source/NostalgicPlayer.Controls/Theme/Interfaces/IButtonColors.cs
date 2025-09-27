c753e6e48d9d0987e47a257655b69ecf0d941cf0
Thomas Neumann <thomas@polycode.dk>
2025-09-27 08:43:14 +0200

    Fixed warnings

--- Source/NostalgicPlayer.Controls/Theme/Interfaces/IButtonColors.cs
+++ Source/NostalgicPlayer.Controls/Theme/Interfaces/IButtonColors.cs
@@ -12,29 +12,49 @@ namespace Polycode.NostalgicPlayer.Controls.Theme.Interfaces
 	/// </summary>
 	public interface IButtonColors
 	{
+		/// <summary></summary>
 		Color NormalBorderColor { get; }
+		/// <summary></summary>
 		Color NormalBackgroundStartColor { get; }
+		/// <summary></summary>
 		Color NormalBackgroundStopColor { get; }
+		/// <summary></summary>
 		Color NormalTextColor { get; }
 
+		/// <summary></summary>
 		Color HoverBorderColor { get; }
+		/// <summary></summary>
 		Color HoverBackgroundStartColor { get; }
+		/// <summary></summary>
 		Color HoverBackgroundStopColor { get; }
+		/// <summary></summary>
 		Color HoverTextColor { get; }
 
+		/// <summary></summary>
 		Color PressedBorderColor { get; }
+		/// <summary></summary>
 		Color PressedBackgroundStartColor { get; }
+		/// <summary></summary>
 		Color PressedBackgroundStopColor { get; }
+		/// <summary></summary>
 		Color PressedTextColor { get; }
 
+		/// <summary></summary>
 		Color FocusedBorderColor { get; }
+		/// <summary></summary>
 		Color FocusedBackgroundStartColor { get; }
+		/// <summary></summary>
 		Color FocusedBackgroundStopColor { get; }
+		/// <summary></summary>
 		Color FocusedTextColor { get; }
 
+		/// <summary></summary>
 		Color DisabledBorderColor { get; }
+		/// <summary></summary>
 		Color DisabledBackgroundStartColor { get; }
+		/// <summary></summary>
 		Color DisabledBackgroundStopColor { get; }
+		/// <summary></summary>
 		Color DisabledTextColor { get; }
 	}
 }
