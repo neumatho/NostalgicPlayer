cc7b29a8d7b1373aee2bf6ef18aa4572dc84fc5f
Thomas Neumann <thomas@polycode.dk>
2025-09-28 08:54:59 +0200

    Added new FontConfiguration component, so it is possible to change font on themed controls.

---
+++ Source/NostalgicPlayer.Controls/Containers/FontType.cs
@@ -0,0 +1,18 @@
+﻿/******************************************************************************/
+/* This source, or parts thereof, may be used in any software as long the     */
+/* license of NostalgicPlayer is keep. See the LICENSE file for more          */
+/* information.                                                               */
+/******************************************************************************/
+namespace Polycode.NostalgicPlayer.Controls.Containers
+{
+	/// <summary>
+	/// Holds the different font types to use
+	/// </summary>
+	public enum FontType
+	{
+		/// <summary></summary>
+		Regular,
+		/// <summary></summary>
+		Monospace,
+	}
+}
