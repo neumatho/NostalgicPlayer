f8006fa6599923e93567e2be8adc520bfb58b935
OliverI79 <oliver@oliver-ickler.de>
2025-11-24 18:12:45 +0100

    Equalizer Release Candidate 3

--- Source/NostalgicPlayer.Library/Sound/Equalizer/EqualizerPresets.cs
+++ Source/NostalgicPlayer.Library/Sound/Equalizer/EqualizerPresets.cs
@@ -7,22 +7,89 @@ namespace Polycode.NostalgicPlayer.Library.Sound.Equalizer
 	/// </summary>
 	public enum EqualizerPreset
 	{
-		Custom, // User-defined values
+		/// <summary>
+		/// User-defined custom values
+		/// </summary>
+		Custom,
+
+		/// <summary>
+		/// Flat response - no equalization
+		/// </summary>
 		Flat,
+
+		/// <summary>
+		/// Rock music preset
+		/// </summary>
 		Rock,
+
+		/// <summary>
+		/// Pop music preset
+		/// </summary>
 		Pop,
+
+		/// <summary>
+		/// Classical music preset
+		/// </summary>
 		Classical,
+
+		/// <summary>
+		/// Jazz music preset
+		/// </summary>
 		Jazz,
+
+		/// <summary>
+		/// Electronic music preset
+		/// </summary>
 		Electronic,
+
+		/// <summary>
+		/// Dance music preset
+		/// </summary>
 		Dance,
+
+		/// <summary>
+		/// Hip-Hop music preset
+		/// </summary>
 		HipHop,
+
+		/// <summary>
+		/// Metal music preset
+		/// </summary>
 		Metal,
+
+		/// <summary>
+		/// Acoustic music preset
+		/// </summary>
 		Acoustic,
+
+		/// <summary>
+		/// Bass boost preset
+		/// </summary>
 		BassBoost,
+
+		/// <summary>
+		/// Treble boost preset
+		/// </summary>
 		TrebleBoost,
+
+		/// <summary>
+		/// Vocal boost preset
+		/// </summary>
 		VocalBoost,
+
+		/// <summary>
+		/// Full bass emphasis preset
+		/// </summary>
 		FullBass,
+
+		/// <summary>
+		/// Full treble emphasis preset
+		/// </summary>
 		FullTreble,
+
+		/// <summary>
+		/// Soft/gentle sound preset
+		/// </summary>
 		Soft
 	}
 
