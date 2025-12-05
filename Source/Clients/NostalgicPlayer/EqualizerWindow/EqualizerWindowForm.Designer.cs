d857546bfc244e8a8ff197a71ca069e61318d603
Thomas Neumann <thomas@polycode.dk>
2025-12-05 16:29:26 +0100

    Small adjustments to the Equalizer window.

--- Source/Clients/NostalgicPlayer/EqualizerWindow/EqualizerWindowForm.Designer.cs
+++ Source/Clients/NostalgicPlayer/EqualizerWindow/EqualizerWindowForm.Designer.cs
@@ -29,44 +29,41 @@ namespace Polycode.NostalgicPlayer.Client.GuiPlayer.EqualizerWindow
 		private void InitializeComponent()
 		{
 			components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(EqualizerWindowForm));
 			fontPalette = new Polycode.NostalgicPlayer.Kit.Gui.Components.FontPalette(components);
 			equalizerPanel = new System.Windows.Forms.Panel();
 			SuspendLayout();
-
-			//
+			// 
 			// fontPalette
-			//
+			// 
 			fontPalette.BaseFont = new System.Drawing.Font("Segoe UI", 9F);
 			fontPalette.BasePaletteType = Krypton.Toolkit.BasePaletteType.Custom;
 			fontPalette.ThemeName = "";
 			fontPalette.UseKryptonFileDialogs = true;
-
-			//
+			// 
 			// equalizerPanel
-			//
+			// 
 			equalizerPanel.BackColor = System.Drawing.Color.White;
 			equalizerPanel.Dock = System.Windows.Forms.DockStyle.Fill;
-			equalizerPanel.Location = new System.Drawing.Point(8, 8);
+			equalizerPanel.Location = new System.Drawing.Point(8, 0);
 			equalizerPanel.Name = "equalizerPanel";
 			equalizerPanel.Size = new System.Drawing.Size(487, 218);
 			equalizerPanel.TabIndex = 0;
-
-			//
+			// 
 			// EqualizerWindowForm
-			//
+			// 
 			AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
 			BackColor = System.Drawing.Color.White;
 			ClientSize = new System.Drawing.Size(503, 226);
 			Controls.Add(equalizerPanel);
-			FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+			FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			Icon = (System.Drawing.Icon)resources.GetObject("$this.Icon");
 			MaximizeBox = false;
 			MinimizeBox = false;
 			Name = "EqualizerWindowForm";
 			Padding = new System.Windows.Forms.Padding(8, 0, 8, 8);
 			Palette = fontPalette;
 			PaletteMode = Krypton.Toolkit.PaletteMode.Custom;
-			StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-			Text = "Equalizer";
 			FormClosed += EqualizerWindowForm_FormClosed;
 			Shown += EqualizerWindowForm_Shown;
 			ResumeLayout(false);
