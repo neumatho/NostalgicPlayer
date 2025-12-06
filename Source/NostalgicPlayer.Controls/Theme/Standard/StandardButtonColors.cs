6744e9e917e6b347d9d2723e7469c5151a50a226
Thomas Neumann <thomas@polycode.dk>
2025-12-06 13:15:37 +0100

    Renamed Office2010Blue to Standard.

--- Source/NostalgicPlayer.Controls/Theme/Standard/StandardButtonColors.cs
+++ Source/NostalgicPlayer.Controls/Theme/Standard/StandardButtonColors.cs
@@ -1,4 +1,4 @@
-﻿/******************************************************************************/
+/******************************************************************************/
 /* This source, or parts thereof, may be used in any software as long the     */
 /* license of NostalgicPlayer is keep. See the LICENSE file for more          */
 /* information.                                                               */
@@ -6,52 +6,44 @@
 using System.Drawing;
 using Polycode.NostalgicPlayer.Controls.Theme.Interfaces;
 
-namespace Polycode.NostalgicPlayer.Controls.Theme.Office2010Blue
+namespace Polycode.NostalgicPlayer.Controls.Theme.Standard
 {
 	/// <summary>
-	/// Different colors used by forms
+	/// Different colors used by buttons
 	/// </summary>
-	internal class Office2010BlueFormColors : IFormColors
+	internal class StandardButtonColors : IButtonColors
 	{
-		private static readonly Color activatedFormOuterColor = Color.FromArgb(144, 154, 166);
-		private static readonly Color activatedFormMiddleColor = Color.FromArgb(212, 230, 245);
-		private static readonly Color activatedFormInnerStartColor = Color.FromArgb(193, 212, 236);
-		private static readonly Color activatedFormInnerStopColor = Color.FromArgb(187, 206, 230);
-		private static readonly Color activatedWindowTitleColor = Color.FromArgb(30, 57, 91);
+		private static readonly Color normalBorderColor = Color.FromArgb(171, 186, 208);
+		private static readonly Color normalBackgroundStartColor = Color.FromArgb(225, 237, 250);
+		private static readonly Color normalBackgroundStopColor = Color.FromArgb(208, 223, 238);
+		private static readonly Color normalTextColor = Color.FromArgb(30, 57, 91);
 
-		private static readonly Color deactivatedFormOuterColor = Color.FromArgb(162, 173, 185);
-		private static readonly Color deactivatedFormMiddleColor = Color.FromArgb(223, 235, 247);
-		private static readonly Color deactivatedFormInnerStartColor = Color.FromArgb(223, 235, 247);
-		private static readonly Color deactivatedFormInnerStopColor = Color.FromArgb(223, 235, 247);
-		private static readonly Color deactivatedWindowTitleColor = Color.FromArgb(106, 128, 168);
+		private static readonly Color hoverBorderColor = Color.FromArgb(237, 202, 87);
+		private static readonly Color hoverBackgroundStartColor = Color.FromArgb(249, 227, 136);
+		private static readonly Color hoverBackgroundStopColor = Color.FromArgb(255, 237, 136);
+		private static readonly Color hoverTextColor = Color.FromArgb(30, 57, 91);
 
-		private static readonly Color closeCaptionButtonHoverStartColor = Color.FromArgb(255, 132, 130);
-		private static readonly Color closeCaptionButtonHoverStopColor = Color.FromArgb(227, 97, 98);
-		private static readonly Color closeCaptionButtonPressStartColor = Color.FromArgb(242, 119, 118);
-		private static readonly Color closeCaptionButtonPressStopColor = Color.FromArgb(206, 85, 84);
+		private static readonly Color pressedBorderColor = Color.FromArgb(227, 182, 67);
+		private static readonly Color pressedBackgroundStartColor = Color.FromArgb(229, 207, 116);
+		private static readonly Color pressedBackgroundStopColor = Color.FromArgb(235, 217, 116);
+		private static readonly Color pressedTextColor = Color.FromArgb(30, 57, 91);
 
-		private static readonly Color captionButtonHoverStartColor = Color.FromArgb(214, 234, 255);
-		private static readonly Color captionButtonHoverStopColor = Color.FromArgb(188, 207, 231);
-		private static readonly Color captionButtonPressStartColor = Color.FromArgb(187, 206, 230);
-		private static readonly Color captionButtonPressStopColor = Color.FromArgb(166, 182, 213);
+		private static readonly Color focusedBorderColor = Color.FromArgb(117, 144, 175);
+		private static readonly Color focusedBackgroundStartColor = Color.FromArgb(255, 255, 255);
+		private static readonly Color focusedBackgroundStopColor = Color.FromArgb(210, 229, 250);
+		private static readonly Color focusedTextColor = Color.FromArgb(30, 57, 91);
 
-		private static readonly Color formBackgroundColor = Color.FromArgb(240, 240, 240);
-
-		/********************************************************************/
-		/// <summary>
-		/// 
-		/// </summary>
-		/********************************************************************/
-		public Color ActivatedFormOuterColor => activatedFormOuterColor;
-
-
+		private static readonly Color disabledBorderColor = Color.FromArgb(180, 180, 180);
+		private static readonly Color disabledBackgroundStartColor = Color.FromArgb(235, 235, 235);
+		private static readonly Color disabledBackgroundStopColor = Color.FromArgb(235, 235, 235);
+		private static readonly Color disabledTextColor = Color.FromArgb(168, 168, 168);
 
 		/********************************************************************/
 		/// <summary>
 		/// 
 		/// </summary>
 		/********************************************************************/
-		public Color ActivatedFormMiddleColor => activatedFormMiddleColor;
+		public Color NormalBorderColor => normalBorderColor;
 
 
 
@@ -60,7 +52,7 @@ namespace Polycode.NostalgicPlayer.Controls.Theme.Office2010Blue
 		/// 
 		/// </summary>
 		/********************************************************************/
-		public Color ActivatedFormInnerStartColor => activatedFormInnerStartColor;
+		public Color NormalBackgroundStartColor => normalBackgroundStartColor;
 
 
 
@@ -69,7 +61,7 @@ namespace Polycode.NostalgicPlayer.Controls.Theme.Office2010Blue
 		/// 
 		/// </summary>
 		/********************************************************************/
-		public Color ActivatedFormInnerStopColor => activatedFormInnerStopColor;
+		public Color NormalBackgroundStopColor => normalBackgroundStopColor;
 
 
 
@@ -78,7 +70,7 @@ namespace Polycode.NostalgicPlayer.Controls.Theme.Office2010Blue
 		/// 
 		/// </summary>
 		/********************************************************************/
-		public Color ActivatedWindowTitleColor => activatedWindowTitleColor;
+		public Color NormalTextColor => normalTextColor;
 
 
 
@@ -87,7 +79,7 @@ namespace Polycode.NostalgicPlayer.Controls.Theme.Office2010Blue
 		/// 
 		/// </summary>
 		/********************************************************************/
-		public Color DeactivatedFormOuterColor => deactivatedFormOuterColor;
+		public Color HoverBorderColor => hoverBorderColor;
 
 
 
@@ -96,7 +88,7 @@ namespace Polycode.NostalgicPlayer.Controls.Theme.Office2010Blue
 		/// 
 		/// </summary>
 		/********************************************************************/
-		public Color DeactivatedFormMiddleColor => deactivatedFormMiddleColor;
+		public Color HoverBackgroundStartColor => hoverBackgroundStartColor;
 
 
 
@@ -105,7 +97,7 @@ namespace Polycode.NostalgicPlayer.Controls.Theme.Office2010Blue
 		/// 
 		/// </summary>
 		/********************************************************************/
-		public Color DeactivatedFormInnerStartColor => deactivatedFormInnerStartColor;
+		public Color HoverBackgroundStopColor => hoverBackgroundStopColor;
 
 
 
@@ -114,7 +106,7 @@ namespace Polycode.NostalgicPlayer.Controls.Theme.Office2010Blue
 		/// 
 		/// </summary>
 		/********************************************************************/
-		public Color DeactivatedFormInnerStopColor => deactivatedFormInnerStopColor;
+		public Color HoverTextColor => hoverTextColor;
 
 
 
@@ -123,7 +115,7 @@ namespace Polycode.NostalgicPlayer.Controls.Theme.Office2010Blue
 		/// 
 		/// </summary>
 		/********************************************************************/
-		public Color DeactivatedWindowTitleColor => deactivatedWindowTitleColor;
+		public Color PressedBorderColor => pressedBorderColor;
 
 
 
@@ -132,7 +124,7 @@ namespace Polycode.NostalgicPlayer.Controls.Theme.Office2010Blue
 		/// 
 		/// </summary>
 		/********************************************************************/
-		public Color CloseCaptionButtonHoverStartColor => closeCaptionButtonHoverStartColor;
+		public Color PressedBackgroundStartColor => pressedBackgroundStartColor;
 
 
 
@@ -141,7 +133,7 @@ namespace Polycode.NostalgicPlayer.Controls.Theme.Office2010Blue
 		/// 
 		/// </summary>
 		/********************************************************************/
-		public Color CloseCaptionButtonHoverStopColor => closeCaptionButtonHoverStopColor;
+		public Color PressedBackgroundStopColor => pressedBackgroundStopColor;
 
 
 
@@ -150,7 +142,7 @@ namespace Polycode.NostalgicPlayer.Controls.Theme.Office2010Blue
 		/// 
 		/// </summary>
 		/********************************************************************/
-		public Color CloseCaptionButtonPressStartColor => closeCaptionButtonPressStartColor;
+		public Color PressedTextColor => pressedTextColor;
 
 
 
@@ -159,7 +151,7 @@ namespace Polycode.NostalgicPlayer.Controls.Theme.Office2010Blue
 		/// 
 		/// </summary>
 		/********************************************************************/
-		public Color CloseCaptionButtonPressStopColor => closeCaptionButtonPressStopColor;
+		public Color FocusedBorderColor => focusedBorderColor;
 
 
 
@@ -168,7 +160,7 @@ namespace Polycode.NostalgicPlayer.Controls.Theme.Office2010Blue
 		/// 
 		/// </summary>
 		/********************************************************************/
-		public Color CaptionButtonHoverStartColor => captionButtonHoverStartColor;
+		public Color FocusedBackgroundStartColor => focusedBackgroundStartColor;
 
 
 
@@ -177,7 +169,7 @@ namespace Polycode.NostalgicPlayer.Controls.Theme.Office2010Blue
 		/// 
 		/// </summary>
 		/********************************************************************/
-		public Color CaptionButtonHoverStopColor => captionButtonHoverStopColor;
+		public Color FocusedBackgroundStopColor => focusedBackgroundStopColor;
 
 
 
@@ -186,7 +178,7 @@ namespace Polycode.NostalgicPlayer.Controls.Theme.Office2010Blue
 		/// 
 		/// </summary>
 		/********************************************************************/
-		public Color CaptionButtonPressStartColor => captionButtonPressStartColor;
+		public Color FocusedTextColor => focusedTextColor;
 
 
 
@@ -195,7 +187,7 @@ namespace Polycode.NostalgicPlayer.Controls.Theme.Office2010Blue
 		/// 
 		/// </summary>
 		/********************************************************************/
-		public Color CaptionButtonPressStopColor => captionButtonPressStopColor;
+		public Color DisabledBorderColor => disabledBorderColor;
 
 
 
@@ -204,6 +196,24 @@ namespace Polycode.NostalgicPlayer.Controls.Theme.Office2010Blue
 		/// 
 		/// </summary>
 		/********************************************************************/
-		public Color FormBackgroundColor => formBackgroundColor;
+		public Color DisabledBackgroundStartColor => disabledBackgroundStartColor;
+
+
+
+		/********************************************************************/
+		/// <summary>
+		/// 
+		/// </summary>
+		/********************************************************************/
+		public Color DisabledBackgroundStopColor => disabledBackgroundStopColor;
+
+
+
+		/********************************************************************/
+		/// <summary>
+		/// 
+		/// </summary>
+		/********************************************************************/
+		public Color DisabledTextColor => disabledTextColor;
 	}
 }
