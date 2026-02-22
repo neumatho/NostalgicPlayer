65f3d04688b48ed6e23dc7780dcac343fd6536f1
Thomas Neumann <thomas@polycode.dk>
2026-02-22 16:29:38 +0100

    Added NostalgicBox control and use it in the DataGridView control to make the border.

---
+++ Source/NostalgicPlayer.Controls/Theme/Interfaces/IBoxColors.cs
@@ -0,0 +1,18 @@
+﻿/******************************************************************************/
+/* This source, or parts thereof, may be used in any software as long the     */
+/* license of NostalgicPlayer is keep. See the LICENSE file for more          */
+/* information.                                                               */
+/******************************************************************************/
+using System.Drawing;
+
+namespace Polycode.NostalgicPlayer.Controls.Theme.Interfaces
+{
+	/// <summary>
+	/// Holds all the colors used by a Box container
+	/// </summary>
+	public interface IBoxColors
+	{
+		/// <summary></summary>
+		Color BorderColor { get; }
+	}
+}
