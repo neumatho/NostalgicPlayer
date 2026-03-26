3cdf7cc94c521c79e0eaa7411a1a87dd983dc8fa
Thomas Neumann <thomas@polycode.dk>
2026-03-26 17:47:35 +0100

    Prepared for hotfix release.

--- Source/NostalgicPlayer.Kit/Interfaces/IAgent.cs
+++ Source/NostalgicPlayer.Kit/Interfaces/IAgent.cs
@@ -17,7 +17,7 @@ namespace Polycode.NostalgicPlayer.Kit.Interfaces
 		/// This is used to ensure that the agent is compatible with the
 		/// current version of NostalgicPlayer
 		/// </summary>
-		public const int NostalgicPlayer_Current_Version = (3 << 16) + (3 << 8) + 0;
+		const int NostalgicPlayer_Current_Version = (3 << 16) + (3 << 8) + 1;
 
 		/// <summary>
 		/// Return the NostalgicPlayer_Current_Version constant defined above
