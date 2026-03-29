fc6ff9392dcd6bde4c49beb98ee67d645a94f76d
Thomas Neumann <thomas@polycode.dk>
2026-03-29 15:40:32 +0200

    Added NostalgicNumberTextBox control.

---
+++ Source/NostalgicPlayer.Controls/Input/NostalgicNumberTextBox.cs
@@ -0,0 +1,45 @@
+﻿/******************************************************************************/
+/* This source, or parts thereof, may be used in any software as long the     */
+/* license of NostalgicPlayer is keep. See the LICENSE file for more          */
+/* information.                                                               */
+/******************************************************************************/
+using System;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace Polycode.NostalgicPlayer.Controls.Input
+{
+	/// <summary>
+	/// Themed number text box with custom rendering
+	/// </summary>
+	public class NostalgicNumberTextBox : NostalgicTextBox
+	{
+		/********************************************************************/
+		/// <summary>
+		/// Is called when a key is pressed
+		/// </summary>
+		/********************************************************************/
+		protected override void OnKeyPress(KeyPressEventArgs e)
+		{
+			if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+				e.Handled = true;
+
+			base.OnKeyPress(e);
+		}
+
+
+
+		/********************************************************************/
+		/// <summary>
+		/// Is called when the text has been changed
+		/// </summary>
+		/********************************************************************/
+		protected override void OnTextChanged(EventArgs e)
+		{
+			if (!Regex.IsMatch(Text, "^[0-9]*"))
+				Text = string.Empty;
+
+			base.OnTextChanged(e);
+		}
+	}
+}
