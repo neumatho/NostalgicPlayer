5b483862153a66015a6c88348e5044fe3f123dfe
Thomas Neumann <thomas@polycode.dk>
2026-05-24 13:25:16 +0200

    Fixed the About window.

---
+++ Source/NostalgicPlayer.Controls/Images/IGeneralImages.cs
@@ -0,0 +1,20 @@
+﻿/******************************************************************************/
+/* This source, or parts thereof, may be used in any software as long the     */
+/* license of NostalgicPlayer is keep. See the LICENSE file for more          */
+/* information.                                                               */
+/******************************************************************************/
+using System.Drawing;
+
+namespace Polycode.NostalgicPlayer.Controls.Images
+{
+	/// <summary>
+	/// Holds all the general usable images
+	/// </summary>
+	public interface IGeneralImages
+	{
+		/// <summary>
+		/// Gets the logo image
+		/// </summary>
+		Bitmap Logo { get; }
+	}
+}
