4c7d74a133b2c0d6b98d90c158864e28b6887d88
Thomas Neumann <thomas@polycode.dk>
2026-06-05 15:58:42 +0200

    Fixed OpenUrl window.

--- Source/Clients/NostalgicPlayer/Windows/OpenUrlWindow/OpenUrlWindowForm.Designer.cs
+++ Source/Clients/NostalgicPlayer/Windows/OpenUrlWindow/OpenUrlWindowForm.Designer.cs
@@ -28,26 +28,17 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			components = new System.ComponentModel.Container();
 			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(OpenUrlWindowForm));
-			fontPalette = new Polycode.NostalgicPlayer.Kit.Gui.Components.FontPalette(components);
 			controlResource = new Polycode.NostalgicPlayer.Kit.Gui.Designer.ControlResource();
-			nameLabel = new Krypton.Toolkit.KryptonLabel();
-			nameTextBox = new Krypton.Toolkit.KryptonTextBox();
-			cancelButton = new Krypton.Toolkit.KryptonButton();
-			playButton = new Krypton.Toolkit.KryptonButton();
-			addButton = new Krypton.Toolkit.KryptonButton();
-			urlTextBox = new Krypton.Toolkit.KryptonTextBox();
-			urlLabel = new Krypton.Toolkit.KryptonLabel();
+			nameLabel = new Polycode.NostalgicPlayer.Controls.Texts.NostalgicLabel();
+			cancelButton = new Polycode.NostalgicPlayer.Controls.Buttons.NostalgicButton();
+			playButton = new Polycode.NostalgicPlayer.Controls.Buttons.NostalgicButton();
+			addButton = new Polycode.NostalgicPlayer.Controls.Buttons.NostalgicButton();
+			urlLabel = new Polycode.NostalgicPlayer.Controls.Texts.NostalgicLabel();
+			nameTextBox = new Polycode.NostalgicPlayer.Controls.Inputs.NostalgicTextBox();
+			urlTextBox = new Polycode.NostalgicPlayer.Controls.Inputs.NostalgicTextBox();
 			((System.ComponentModel.ISupportInitialize)controlResource).BeginInit();
 			SuspendLayout();
-			// 
-			// fontPalette
-			// 
-			fontPalette.BaseFont = new System.Drawing.Font("Segoe UI", 9F);
-			fontPalette.BasePaletteType = Krypton.Toolkit.BasePaletteType.Custom;
-			fontPalette.ThemeName = "";
-			fontPalette.UseKryptonFileDialogs = true;
 			// 
 			// controlResource
 			// 
@@ -57,23 +48,10 @@
 			// 
 			nameLabel.Location = new System.Drawing.Point(8, 8);
 			nameLabel.Name = "nameLabel";
-			nameLabel.Palette = fontPalette;
-			nameLabel.PaletteMode = Krypton.Toolkit.PaletteMode.Custom;
 			controlResource.SetResourceKey(nameLabel, "IDS_OPENURL_NAME");
 			nameLabel.Size = new System.Drawing.Size(232, 16);
 			nameLabel.TabIndex = 0;
-			nameLabel.Values.Text = "Enter the name to be shown in the module list";
-			// 
-			// nameTextBox
-			// 
-			nameTextBox.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
-			nameTextBox.Location = new System.Drawing.Point(8, 24);
-			nameTextBox.Name = "nameTextBox";
-			nameTextBox.Palette = fontPalette;
-			nameTextBox.PaletteMode = Krypton.Toolkit.PaletteMode.Custom;
-			controlResource.SetResourceKey(nameTextBox, null);
-			nameTextBox.Size = new System.Drawing.Size(342, 20);
-			nameTextBox.TabIndex = 1;
+			nameLabel.Text = "Enter the name to be shown in the module list";
 			// 
 			// cancelButton
 			// 
@@ -84,7 +62,7 @@
 			controlResource.SetResourceKey(cancelButton, "IDS_OPENURL_BUTTON_CANCEL");
 			cancelButton.Size = new System.Drawing.Size(90, 25);
 			cancelButton.TabIndex = 6;
-			cancelButton.Values.Text = "Cancel";
+			cancelButton.Text = "Cancel";
 			// 
 			// playButton
 			// 
@@ -95,7 +73,7 @@
 			controlResource.SetResourceKey(playButton, "IDS_OPENURL_BUTTON_PLAY");
 			playButton.Size = new System.Drawing.Size(90, 25);
 			playButton.TabIndex = 5;
-			playButton.Values.Text = "Play";
+			playButton.Text = "Play";
 			// 
 			// addButton
 			// 
@@ -106,34 +84,37 @@
 			controlResource.SetResourceKey(addButton, "IDS_OPENURL_BUTTON_ADD");
 			addButton.Size = new System.Drawing.Size(90, 25);
 			addButton.TabIndex = 4;
-			addButton.Values.Text = "Add";
+			addButton.Text = "Add";
+			// 
+			// urlLabel
+			// 
+			urlLabel.Location = new System.Drawing.Point(8, 56);
+			urlLabel.Name = "urlLabel";
+			controlResource.SetResourceKey(urlLabel, "IDS_OPENURL_URL");
+			urlLabel.Size = new System.Drawing.Size(243, 16);
+			urlLabel.TabIndex = 2;
+			urlLabel.Text = "Enter an URL to the stream you want to listen to";
+			// 
+			// nameTextBox
+			// 
+			nameTextBox.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
+			nameTextBox.Location = new System.Drawing.Point(8, 24);
+			nameTextBox.Name = "nameTextBox";
+			nameTextBox.Size = new System.Drawing.Size(342, 21);
+			nameTextBox.TabIndex = 1;
 			// 
 			// urlTextBox
 			// 
 			urlTextBox.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
 			urlTextBox.Location = new System.Drawing.Point(8, 72);
 			urlTextBox.Name = "urlTextBox";
-			urlTextBox.Palette = fontPalette;
-			urlTextBox.PaletteMode = Krypton.Toolkit.PaletteMode.Custom;
-			controlResource.SetResourceKey(urlTextBox, null);
-			urlTextBox.Size = new System.Drawing.Size(342, 20);
+			urlTextBox.Size = new System.Drawing.Size(342, 21);
 			urlTextBox.TabIndex = 3;
-			// 
-			// urlLabel
-			// 
-			urlLabel.Location = new System.Drawing.Point(8, 56);
-			urlLabel.Name = "urlLabel";
-			urlLabel.Palette = fontPalette;
-			urlLabel.PaletteMode = Krypton.Toolkit.PaletteMode.Custom;
-			controlResource.SetResourceKey(urlLabel, "IDS_OPENURL_URL");
-			urlLabel.Size = new System.Drawing.Size(243, 16);
-			urlLabel.TabIndex = 2;
-			urlLabel.Values.Text = "Enter an URL to the stream you want to listen to";
 			// 
 			// OpenUrlWindowForm
 			// 
 			AcceptButton = addButton;
-			AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+			AllowResizing = false;
 			CancelButton = cancelButton;
 			ClientSize = new System.Drawing.Size(362, 137);
 			Controls.Add(urlTextBox);
@@ -143,31 +124,24 @@
 			Controls.Add(cancelButton);
 			Controls.Add(nameTextBox);
 			Controls.Add(nameLabel);
-			FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
 			Icon = (System.Drawing.Icon)resources.GetObject("$this.Icon");
 			MaximizeBox = false;
 			MinimizeBox = false;
 			Name = "OpenUrlWindowForm";
-			Palette = fontPalette;
-			PaletteMode = Krypton.Toolkit.PaletteMode.Custom;
-			controlResource.SetResourceKey(this, null);
 			ShowInTaskbar = false;
 			StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
 			((System.ComponentModel.ISupportInitialize)controlResource).EndInit();
 			ResumeLayout(false);
-			PerformLayout();
 		}
 
 		#endregion
-
-		private Kit.Gui.Components.FontPalette fontPalette;
 		private Kit.Gui.Designer.ControlResource controlResource;
-		private Krypton.Toolkit.KryptonLabel nameLabel;
-		private Krypton.Toolkit.KryptonTextBox nameTextBox;
-		private Krypton.Toolkit.KryptonButton cancelButton;
-		private Krypton.Toolkit.KryptonButton playButton;
-		private Krypton.Toolkit.KryptonButton addButton;
-		private Krypton.Toolkit.KryptonTextBox urlTextBox;
-		private Krypton.Toolkit.KryptonLabel urlLabel;
+		private NostalgicPlayer.Controls.Texts.NostalgicLabel nameLabel;
+		private NostalgicPlayer.Controls.Inputs.NostalgicTextBox nameTextBox;
+		private NostalgicPlayer.Controls.Buttons.NostalgicButton cancelButton;
+		private NostalgicPlayer.Controls.Buttons.NostalgicButton playButton;
+		private NostalgicPlayer.Controls.Buttons.NostalgicButton addButton;
+		private NostalgicPlayer.Controls.Inputs.NostalgicTextBox urlTextBox;
+		private NostalgicPlayer.Controls.Texts.NostalgicLabel urlLabel;
 	}
 }
