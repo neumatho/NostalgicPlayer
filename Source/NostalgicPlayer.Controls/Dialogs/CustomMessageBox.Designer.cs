b279abf677477afab979c00565393da5d1005150
Thomas Neumann <thomas@polycode.dk>
2026-06-08 18:25:32 +0200

    Moved CustomMessageBox into the Controls project and themed it.

--- Source/NostalgicPlayer.Controls/Dialogs/CustomMessageBox.Designer.cs
+++ Source/NostalgicPlayer.Controls/Dialogs/CustomMessageBox.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace Polycode.NostalgicPlayer.Kit.Gui.Controls
+﻿namespace Polycode.NostalgicPlayer.Controls.Dialogs
 {
 	partial class CustomMessageBox
 	{
@@ -28,9 +28,7 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			components = new System.ComponentModel.Container();
-			messageLabel = new Krypton.Toolkit.KryptonLabel();
-			fontPalette = new Polycode.NostalgicPlayer.Kit.Gui.Components.FontPalette(components);
+			messageLabel = new Polycode.NostalgicPlayer.Controls.Texts.NostalgicLabel();
 			pictureBox = new System.Windows.Forms.PictureBox();
 			messagePanel = new System.Windows.Forms.Panel();
 			buttonPanel = new System.Windows.Forms.Panel();
@@ -40,22 +38,13 @@
 			// 
 			// messageLabel
 			// 
-			messageLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+			messageLabel.AutoSize = true;
 			messageLabel.Location = new System.Drawing.Point(0, 0);
 			messageLabel.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
 			messageLabel.Name = "messageLabel";
-			messageLabel.Palette = fontPalette;
-			messageLabel.PaletteMode = Krypton.Toolkit.PaletteMode.Custom;
-			messageLabel.Size = new System.Drawing.Size(40, 16);
+			messageLabel.Size = new System.Drawing.Size(35, 13);
 			messageLabel.TabIndex = 0;
-			messageLabel.Values.Text = "label1";
-			// 
-			// fontPalette
-			// 
-			fontPalette.BaseFont = new System.Drawing.Font("Segoe UI", 9F);
-			fontPalette.BasePaletteType = Krypton.Toolkit.BasePaletteType.Custom;
-			fontPalette.ThemeName = "";
-			fontPalette.UseKryptonFileDialogs = true;
+			messageLabel.Text = "label1";
 			// 
 			// pictureBox
 			// 
@@ -75,12 +64,12 @@
 			messagePanel.Location = new System.Drawing.Point(56, 15);
 			messagePanel.Margin = new System.Windows.Forms.Padding(0);
 			messagePanel.Name = "messagePanel";
-			messagePanel.Size = new System.Drawing.Size(40, 16);
+			messagePanel.Size = new System.Drawing.Size(39, 16);
 			messagePanel.TabIndex = 3;
 			// 
 			// buttonPanel
 			// 
-			buttonPanel.Anchor = System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right;
+			buttonPanel.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right;
 			buttonPanel.AutoSize = true;
 			buttonPanel.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
 			buttonPanel.Location = new System.Drawing.Point(273, 56);
@@ -91,15 +80,12 @@
 			// 
 			// CustomMessageBox
 			// 
-			AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-			AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			AutoSize = true;
 			AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
 			ClientSize = new System.Drawing.Size(284, 62);
 			Controls.Add(buttonPanel);
 			Controls.Add(messagePanel);
 			Controls.Add(pictureBox);
-			FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
 			Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
 			MaximizeBox = false;
 			MaximumSize = new System.Drawing.Size(581, 340);
@@ -120,10 +106,9 @@
 
 		#endregion
 
-		private Krypton.Toolkit.KryptonLabel messageLabel;
+		private Polycode.NostalgicPlayer.Controls.Texts.NostalgicLabel messageLabel;
 		private System.Windows.Forms.PictureBox pictureBox;
 		private System.Windows.Forms.Panel messagePanel;
 		private System.Windows.Forms.Panel buttonPanel;
-		private Components.FontPalette fontPalette;
 	}
 }
